85848b0d3cd7e7318c7db0edf0b911a89720e016
yyeatgrass <344588135z@gmail.com>
2021-08-03 23:31:04 +0800

    --- Auto-Git Commit ---

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,20 +2,23 @@
 #include "user/user.h"
 
 int
-main(int argc, char *argv[]) {
+main(void) {
    int buf[1];
-   int p[2];
-   pipe(p);
+   int p2c[2];
+   int c2p[2];
+   pipe(p2c);
+   pipe(c2p);
+
    if (fork() == 0) {
-      read(p[0], buf, 1);
-      fprintf(2, "received ping\n");
-      write(p[1], buf, 1);
+      read(p2c[0], buf, 1);
+      fprintf(2, "%d: received ping\n", getpid());
+      write(c2p[1], buf, 1);
       exit(0);
    }
 
-   write(p[1], buf, 1);
-   read(p[0], buf, 1);
-   fprintf(2, "received pong\n");
+   write(p2c[1], buf, 1);
+   read(c2p[0], buf, 1);
+   fprintf(2, "%d: received pong\n", getpid());
    wait(0);
    exit(0);
 }
