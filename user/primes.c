7206e124360c9d7850f0c64df904fed364eb5a66
yyeatgrass <344588135z@gmail.com>
2021-08-06 16:41:54 +0800

    --- Auto-Git Commit ---

--- user/primes.c
+++ user/primes.c
@@ -2,15 +2,24 @@
 #include "user/user.h"
 
 
+void processNum(int inChan, int lePrime);
+
 int
 main(void) {
-   int n = 0;
-   while (n <= 35) {
-      write(, n, );
-      if () {
-
-      }
+   int n = 2;
+   int outChan[2];
+   pipe(outChan);
+   fprintf(2, "outChan %d, %d\n", outChan[0], outChan[1]);
+   if (fork() == 0) {
+      processNum(outChan[0], n);
    }
+   fprintf(2, "cccc\n");
+   while (++n <= 35) {
+      fprintf(2, "bbbb\n");
+      write(outChan[1], &n, 1);
+   }
+   wait(0);
+   exit(0);
 }
 
 void
@@ -18,48 +27,27 @@ processNum(int inChan, int lePrime) {
    int buf[1];
    int outChan[2];
    uint chanCreated = 0;
-   while (read(inChan, buf,  ) > 0) {
-      if (buf[0] == 0) {
-         fprintf(2, "prime %d", lePrime);
-         wait(0); // waitpid?
-         exit(0);
-      }
+   fprintf(2, "aaaaa %d\n", lePrime);
+   while (read(inChan, buf, 1) > 0) {
       if (buf[0] % lePrime == 0) {
          continue;
       }
-
       if (!chanCreated) {
          pipe(outChan);
+         fprintf(2, "outChan %d, %d\n", outChan[0], outChan[1]);
          if (fork() == 0) {
             processNum(outChan[0], buf[0]);
          }
          chanCreated = 1;
       } else {
-         write(outChan[1], buf[0], 1);
+         write(outChan[1], buf, 1);
       }
    }
-}
 
-
-
-int
-main(void) {
-   int buf[1];
-   int p2c[2];
-   int c2p[2];
-   pipe(p2c);
-   pipe(c2p);
-
-   if (fork() == 0) {
-      read(p2c[0], buf, 1);
-      fprintf(2, "%d: received ping\n", getpid());
-      write(c2p[1], buf, 1);
-      exit(0);
+   fprintf(2, "prime %d", lePrime);
+   if (chanCreated) {
+      close(outChan[0]);
    }
-
-   write(p2c[1], buf, 1);
-   read(c2p[0], buf, 1);
-   fprintf(2, "%d: received pong\n", getpid());
-   wait(0);
+   wait(0); // waitpid?
    exit(0);
 }
