08a0c3e9f048fec7196fff10e8a6b25d8b93d5c0
yyeatgrass <344588135z@gmail.com>
2021-08-06 23:50:21 +0800

    Add user program primes.c

--- user/primes.c
+++ user/primes.c
@@ -8,19 +8,19 @@ int
 main(void) {
    int n = 2;
    int outChan[2];
+
+   close(0);
+   close(1);
    if (pipe(outChan) < 0) {
       fprintf(2, "pipe failed.");
    };
-   close(0);
-   close(1);
-//   fprintf(2, "outChan %d, %d\n", outChan[0], outChan[1]);
+
    if (fork() == 0) {
+      close(outChan[1]);
       processNum(outChan[0], n);
-      exit(1);
    }
-//   fprintf(2, "cccc\n");
+   close(outChan[0]);
    while (++n <= 35) {
-//      fprintf(2, "bbbb\n");
       write(outChan[1], &n, 1);
    }
    close(outChan[1]);
@@ -34,10 +34,7 @@ processNum(int inChan, int lePrime) {
    int outChan[2];
    uint chanCreated = 0;
 
-   // Free unused file descriptors.
-//   fprintf(2, "aaaaa %d\n", lePrime);
    while (read(inChan, buf, 1) > 0) {
-      fprintf(2, "read num %d in process %d\n", buf[0], getpid());
       if (buf[0] % lePrime == 0) {
          continue;
       }
@@ -46,21 +43,21 @@ processNum(int inChan, int lePrime) {
             fprintf(2, "pipe failed in process %d, lePrime %d.", getpid(), lePrime);
             exit(1);
          };
-         close(0);
-         close(1);
-//         fprintf(2, "outChan %d, %d\n", outChan[0], outChan[1]);
          if (fork() == 0) {
+            close(inChan);
+            close(outChan[1]);
             processNum(outChan[0], buf[0]);
          }
+         close(outChan[0]);
          chanCreated = 1;
       } else {
          write(outChan[1], buf, 1);
       }
    }
 
-   fprintf(2, "prime %d", lePrime);
+   fprintf(2, "prime %d\n", lePrime);
    if (chanCreated) {
-      close(outChan[0]);
+      close(outChan[1]);
    }
    wait(0); // waitpid?
    exit(0);
